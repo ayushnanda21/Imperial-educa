10b223ad68d5d062c308b6888ce51b4cf77879d5
Ayush Nanda <ananda_be18@thapar.edu>
2021-03-28 22:44:17 +0530

    content models using model inheritance & custom fields

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -1,6 +1,11 @@
 from	django.db	import	models
 from	django.contrib.auth.models	import	User
 
+#Imports for diverse content like images,files,videos,text
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+#import end
+
 class	Subject(models.Model):
     title	=	models.CharField(max_length=200)
     slug	=	models.SlugField(max_length=200,	unique=True)
@@ -33,3 +38,37 @@ class	Module(models.Model):
     
     def	__str__(self):
         return	self.title
+
+#model for diverse content START
+
+class Content(models.Model):
+    module = models.ForeignKey(Module , related_name='contents' , on_delete = models.CASCADE)
+    content_type = models.ForeignKey(ContentType , on_delete =models.CASCADE ,limit_choices_to={'model__in' : ('text', 'video' , 'image' , 'file')})
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type' , 'object_id')
+
+#model-inheritance
+
+class ItemBase(models.Model):
+    owner= models.ForeignKey(User , related_name= '%(class)s_related', on_delete =models.CASCADE)
+    title= models.CharField(max_length=250)
+    created =models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+    
+class Text(ItemBase):
+    content =models.TextField()
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+class Image(ItemBase):
+    file =models.FileField(upload_to='images')
+
+class Video(ItemBase):
+    url = models.URLField()
