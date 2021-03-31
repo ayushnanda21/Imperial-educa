47bc19f515ab0b73cbf20fa9bc65f2d36c7973f6
Ayush Nanda <ananda_be18@thapar.edu>
2021-03-31 22:33:01 +0530

    template error solved & permissions rights granted

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView , UpdateView , DeleteView
 from .models import Course
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 # Create your views here.
 class ManagerCourseListView(ListView):
@@ -15,19 +15,21 @@ class ManagerCourseListView(ListView):
         return qs.filter(owner =self.request.user)
 
 class OwnerMixin(object):
-    def def get_queryset(self):
+    def get_queryset(self):
         qs =super(OwnerMixin,self).get_queryset()
         return qs.filter(owner = self.request.user)
         
     
 
-class OwnerEditMixin(Object):
+class OwnerEditMixin(object):
     def form_valid(self,form):
         form.instance.owner = self.request.user
         return super(OwnerEditMixin ,self).form_valid(form)
 
 class OwnerCourseMixin(OwnerMixin):
     model = Course
+    #fields = ['subject' , 'title' , 'slug' , 'overview']
+    #success_url =reverse_lazy('manage_course_list')
 
 class OwnerCourseEditMixin(OwnerCourseMixin ,OwnerEditMixin):
     fields = ['subject' , 'title' , 'slug' , 'overview']
@@ -37,13 +39,14 @@ class OwnerCourseEditMixin(OwnerCourseMixin ,OwnerEditMixin):
 class ManageCourseListView(OwnerCourseMixin, ListView):
     template_name ='courses/manage/course/list.html'
 
-class CourseCreateView(OwnerCourseEditMixin , CreateView):
-    pass
+class CourseCreateView(PermissionRequiredMixin,OwnerCourseEditMixin , CreateView):
+    permission_required = 'courses.add_course'
 
-class CourseUpdateView(OwnerCourseEditMixin , UpdateView):
-    pass
+class CourseUpdateView(PermissionRequiredMixin, OwnerCourseEditMixin , UpdateView):
+    permission_required = 'courses.change_course'
 
-class CourseDeleteView(OwnerCourseMixin ,DeleteView):
+class CourseDeleteView(PermissionRequiredMixin, OwnerCourseMixin ,DeleteView):
     template_name = 'courses/manage/course/delete.html'
     success_url = reverse_lazy('manage_course_list')
+    permission_required ='courses.delete_course'
 
--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 #import for authentication
 from django.contrib.auth import views as auth_views
 
+
 urlpatterns = [
-    path('accounts/login', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
+    path('course/', include('courses.urls')),
 ]
