c8845c48da3cb8379da39cfd31ea27620cee63e6
Ayush Nanda <ananda_be18@thapar.edu>
2021-05-14 15:55:42 +0530

    authentication for api use and custom permissions

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
+from rest_framework.fields import ReadOnlyField
 from ..models import Subject
 from ..models import Course
 from ..models import Module
-
+from ..models import Content
 
 class ModuleSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,9 +16,26 @@ class SubjectSerializer(serializers.ModelSerializer):
         model = Subject
         fields = ['id', 'title', 'slug']
 
+class ItemRelatedField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.render()
+
 class CourseSerializer(serializers.ModelSerializer):
 
+    item = ItemRelatedField(read_only = True)
     modules = ModuleSerializer(many =True, read_only = True)
     class Meta:
         model = Course
         fields = ['id', 'subject', 'title', 'slug', 'overview','created', 'owner', 'modules']
+
+class ModuleWithContentsSerializer(serializers.ModelSerializer):
+    contents = ContentSerializer(many=True)
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description', 'contents']
+
+class CourseWithContentsSerializer(serializers.ModelSerializer):
+    modules = ModuleWithContentsSerializer(many=True)
+    class Meta:
+        model = Course
+        fields = ['id', 'subject', 'title', 'slug','overview', 'created', 'owner', 'modules']
--- courses/api/views.py
+++ courses/api/views.py
@@ -6,6 +6,17 @@ from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from ..models import Course
+from rest_framework.decorators import detail_route
+from .permissions import IsEnrolled
+from .serializers import CourseWithContentsSerializer
+
+#to handle authentication for API
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+#routers
+from rest_framework import viewsets
+from .serializers import CourseSerializer
 
 
 class SubjectListView(generics.ListAPIView):
@@ -21,3 +32,31 @@ class CourseEnrollView(APIView):
         course =  get_object_or_404(Course, pk= pk)
         course.students.add(request.user)
         return response({'enrolled': True})
+
+    authentication_classes = (BasicAuthentication,)
+
+    permission_classes = (IsAuthenticated,)
+
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset =  Course.objects.all()
+    serializer_class= CourseSerializer
+
+    @detail_route(methods=['post'],
+    authentication_classes=[BasicAuthentication],
+    permission_classes=[IsAuthenticated])
+
+
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response({'enrolled': True})
+
+    @detail_route(methods=['get'],
+    serializer_class=CourseWithContentsSerializer,
+    authentication_classes=[BasicAuthentication],
+    permission_classes=[IsAuthenticated,IsEnrolled])
+    
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
